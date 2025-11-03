152ba8c8929179703bbffc1f858704680ffdeb1c
Zephrio <76012925+CD-Chief@users.noreply.github.com>
2025-11-03 21:29:42 +0000

    feat: add code to test turning off backlight

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
+button_pressed = False
 def button_callback(channel):
+    global button_pressed
     print("Button pressed!")
+    button_pressed = True
 
 lcd1602 = CharLCD1602()
 def get_cpu_temp():     # get CPU temperature from file "/sys/class/thermal/thermal_zone0/temp"
@@ -25,6 +27,13 @@ def loop():
     lcd1602.init_lcd()
     count = 0
     while(True):
+        if button_pressed:
+            print("Closing")
+            lcd1602.closelight()
+            sleep(1)
+            print("Opening")
+            lcd1602.openlight()
+            
         lcd1602.clear()
         lcd1602.write(0, 0, 'CPU: ' + get_cpu_temp() )# display CPU temperature
         lcd1602.write(0, 1, get_time_now() )   # display the time
@@ -40,6 +49,7 @@ if __name__ == '__main__':
     GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(19, GPIO.FALLING, callback=button_callback, bouncetime=200)
     print("Added Event")
+
 
     try:
         loop()
